5533a736c85557f19999f3fb38605591d96943d3
yudin-1337 <yudin-1337@yandex.ru>
2025-01-16 14:34:51 +0500

    Sprint 4

--- mochaSetup.js
+++ mochaSetup.js
@@ -2,9 +2,9 @@ import {JSDOM} from 'jsdom'
 
 const jsdom = new JSDOM(`<body></body>`);
 
-global.XMLHttpRequest = jsdom.window.XMLHttpRequest;;
 global.window = jsdom.window;
 global.document = jsdom.window.document;
 global.MouseEvent = jsdom.window.MouseEvent;
 global.Node = jsdom.window.Node;
 global.history = jsdom.window.history;
+global.XMLHttpRequest = jsdom.window.XMLHttpRequest;
